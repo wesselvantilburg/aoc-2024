c75cb7aaef401a5da9de970f1c21fc53bec670b4
Wessel van Tilburg <wessel.vantilburg@iodigital.com>
2024-12-10 08:01:59 +0100

    feat: :white_check_mark: day ten - part one & two

---
+++ src/days/10/two.ts
@@ -0,0 +1,78 @@
+import { duration, fetchInputByDay } from "@utils";
+
+/* -------------------------------------------------------------------------- */
+
+const directions = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+/* -------------------------------------------------------------------------- */
+
+let grid: number[][] = [];
+let trailheads: number[][] = [];
+
+/* -------------------------------------------------------------------------- */
+
+function isOnGrid(position: number[]): boolean {
+  // Y outside bounds
+  if (position[1] < 0 || position[1] > grid.length - 1) return false;
+  // X outside bounds
+  if (position[0] < 0 || position[0] > grid[0].length - 1) return false;
+
+  return true;
+}
+
+function getTrailScore(trailhead: number[]): number {
+  let score = 0;
+
+  function getScore(position: number[]) {
+    if (grid[position[1]][position[0]] === 9) {
+      score += 1;
+      return;
+    }
+
+    directions.forEach((direction) => {
+      const goToX = position[0] + direction[0];
+      const goToY = position[1] + direction[1];
+
+      if (isOnGrid([goToX, goToY])) {
+        if (grid[goToY][goToX] - grid[position[1]][position[0]] === 1) {
+          getScore([goToX, goToY]);
+        }
+      }
+    });
+  }
+
+  getScore(trailhead);
+
+  return score;
+}
+
+/* -------------------------------------------------------------------------- */
+
+function findSolution(input: string): number {
+  grid = input.split("\n").map((line, row) =>
+    line.split("").map((char, col) => {
+      if (char === "0") {
+        trailheads.push([col, row]);
+      }
+      return Number(char);
+    })
+  );
+
+  return trailheads.reduce((total, trailhead) => {
+    return (total += getTrailScore(trailhead));
+  }, 0);
+}
+
+/* -------------------------------------------------------------------------- */
+
+const rawInput = await fetchInputByDay(10);
+const result = duration(findSolution)(rawInput);
+
+/* -------------------------------------------------------------------------- */
+
+console.log(result);
